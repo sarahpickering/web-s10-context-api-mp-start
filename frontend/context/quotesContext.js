4ffde5d1401548c6815735ee9b2604db3a8dd610
Sarah Pickering <sarahpsellsaz@gmail.com>
2024-10-29 09:03:16 -0700

    Set up quotesContext

--- frontend/context/quotesContext.js
+++ frontend/context/quotesContext.js
@@ -1,1 +1,16 @@
-// Create your context here
+import React, { createContext } from 'react'
+
+export const QuotesContext = createContext()
+
+const value = {
+    car: 'Toyota Celica'
+}
+
+export function QuotesProvider(props) {
+
+    return (
+        <QuotesContext.Provider value={value}>
+            {props.children}
+        </QuotesContext.Provider>
+    )
+}
